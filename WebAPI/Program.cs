6fe03a34e4f1a080d005a56fe05eccefcf06da5f
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2024-04-03 13:03:35 +0300

    Middleware yapısı eklendi.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -68,6 +68,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.ConfigureCustomExceptionMiddleware();
 
 app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
